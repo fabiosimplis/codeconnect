3aa912d9fcb550c77bd23bedf23ce17a8bdf8de2
fabiosimplis <fabio.simplis@gmail.com>
2025-01-14 18:36:48 -0300

    add filtro component

---
+++ src/components/Filtro/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export default function Filtro() {
+  return (
+    <section className="container-filtro">
+      <ul>
+        <li>Programaçao</li>
+      </ul>
+      <button>Limpar tudo</button>
+    </section>
+  )
+}
