bd0f1ade0707c6d191e73866986a108e7b952d63
fabiosimplis <fabio.simplis@gmail.com>
2025-01-14 17:51:28 -0300

    add `useState` para capturar o estado da pesquisa

--- src/components/BarraDePesquisa/index.jsx
+++ src/components/BarraDePesquisa/index.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import './style.css';
 
 export default function BarraDePesquisa() {
+  const [termoPesquisa, setTermoPesquisa] = useState('');
+  console.log(termoPesquisa);
   return (
-    <input type="search" placeholder="Digite o que você procura" className='barra-pesquisa'/>
+    <input 
+    type="search" 
+    placeholder="Digite o que você procura" 
+    className='barra-pesquisa'
+    value={termoPesquisa}
+    onChange={(evento) => setTermoPesquisa(evento.target.value)}
+    />
   )
 }
